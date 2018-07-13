26cda6eeb229abd0b22bcf5b9e8e36d0994408f0
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-13 02:57:41 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,7 +45,7 @@ def selective_tweet_shortener(tweets)
 def shortened_tweet_truncator(tweets)
   tweet = tweets
     if tweet.size > 140
-      tweet = word_substituter(tweet[0..139])+"..."
+      tweet = word_substituter(tweet[0..136]) + "..."
     end
   tweets
 end
